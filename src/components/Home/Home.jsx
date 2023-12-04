5cc980a65d17de70c8974b248bfc83ca9802031a
shoaibrasool <shooaib6@gmail.com>
2023-12-04 21:31:13 +0500

    complete homePage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,13 @@ const Home = () => {
   return (
     <div className='heroSect'>
       <div className="hero">
-        Hero Sect
+        <h1>I'm Shoaib RAsool</h1>
+        <h1>I code cool websites</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab fugiat perferendis aut cumque accusantium it</p>
+        <div className="buttons">
+          <button>Portfolio</button>
+          <button>Contact Me</button>
+        </div>
       </div>
       <div className="img">
         
